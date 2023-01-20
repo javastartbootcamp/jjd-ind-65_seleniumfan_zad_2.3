02fde0ecc5744f940d9ca3fc851ad92b1f50ced6
Tomasz Iwanicki <tiko35@o2.pl>
2023-01-21 00:57:36 +0100

    - Adding Drink class which has ingredients (name, amount) - Adding one object of Drink class - Showing drink with ingredients

---
+++ src/main/java/pl/javastart/task/Drink.java
@@ -0,0 +1,10 @@
+package pl.javastart.task;
+
+public class Drink {
+    String name;
+    double price;
+    boolean isAlcoholic;
+    Ingredient ingredient1;
+    Ingredient ingredient2;
+    Ingredient ingredient3;
+}
